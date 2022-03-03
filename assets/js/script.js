a3a9b1f71510618cf7bdf5173e426897415b6d08
sam-levin <sam.ky.levin@gmail.com>
2022-03-03 13:12:50 -0800

    adding drag n drop functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,8 +45,71 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out",event.target);
+  },
+  update: function(event) { 
+    var tempArr = [];
+    // loop over current set of children in sortable list
+    $(this).children().each(function() {
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+    
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+    
+      // add task data to the temp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    });
+    console.log(tempArr)
+    var arrName = $(this)
+      .attr("id")
+      .replace("list-","")
+
+    //update array on tasks object and svae
+    tasks[arrName] = tempArr;
+    saveTasks();
+  }
+});
 
 
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    console.log("drop");
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event,ui){
+    console.log("out");
+  }
+  }
+
+);
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
